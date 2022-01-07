afbe0151635052be06f4c377855fbe6605dd6575
Jiajun Yao <yjj_zj@163.com>
2022-01-07 18:09:32 +0800

    Update

--- python_built-in_methods/gzip/gzip_read.py
+++ python_built-in_methods/gzip/gzip_read.py
@@ -4,8 +4,8 @@ import gzip
 import pandas as pd
 
 
-with gzip.open('/home/yao/BGIpy37_pytorch113/Info_Learning/python_built-in_methods/gzip/deml_fbgn.tsv.gz', 'rb') as input_file:
+with gzip.open('/home/yao/BGIpy37_pytorch113/ST_Drosophila/stDrosophila/data/bdgp_flyfish.tsv.gz', 'rb') as input_file:
     with io.TextIOWrapper(input_file, encoding='utf-8') as dec:
-        dec_list = [str(i).strip("\n").split("\t") for i in dec.readlines()]
+        dec_list = [str(i).strip("\n").split(",") for i in dec.readlines()]
         dec_data = pd.DataFrame(dec_list[1:], columns=dec_list[0], dtype=str)
         print(dec_data)
--- python_built-in_methods/gzip/gzip_write.py
+++ python_built-in_methods/gzip/gzip_write.py
@@ -3,11 +3,11 @@ import io
 import gzip
 import pandas as pd
 
-with open("deml_fbgn.csv", "r") as f:
+with open("/home/yao/BGIpy37_pytorch113/ST_Drosophila/1_Preprocessing/LassoByUbiquitousGenes/filter_flymine.csv", "r") as f:
     content = f.readlines()
     print(content)
 
-with gzip.open('/home/yao/BGIpy37_pytorch113/Info_Learning/python_built-in_methods/gzip/deml_fbgn.tsv.gz', 'wb') as output:
+with gzip.open('/home/yao/BGIpy37_pytorch113/ST_Drosophila/stDrosophila/data/bdgp_flyfish.tsv.gz', 'wb') as output:
     with io.TextIOWrapper(output, encoding='utf-8') as enc:
         enc.writelines(content)
 
